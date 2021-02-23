3bbaa0fde364227fb8ec3eed2aa0718fc5fc4512
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-02-23 15:43:53 +0100

    add days

--- app.js
+++ app.js
@@ -3,10 +3,39 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.send("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error current day is :" + currentDay);
+  }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
